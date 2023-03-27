4a4cd8dba08251fd44172819a0c59a7834a073f0
littlegamer757 <jneissl27@gmail.com>
2023-03-27 09:16:29 +0200

    Implement components

--- projects/core-ui-lib/src/lib/core-ui-lib.module.ts
+++ projects/core-ui-lib/src/lib/core-ui-lib.module.ts
@@ -1,19 +1,53 @@
-import { NgModule } from '@angular/core';
-import { CoreUiLibComponent } from './core-ui-lib.component';
-import { TestComponent } from './test/test.component';
+import {NgModule} from "@angular/core";
+import {SynoSelectMultipleComponent} from "./syno-select-multiple/syno-select-multiple.component";
+import {SynoIconButtonComponent} from "./syno-icon-button/syno-icon-button.component";
+import {SynoSelectComponent} from "./syno-select/syno-select.component";
+import {SynoUsernameComponent} from "./syno-username/syno-username.component";
+import {SynoTagComponent} from "./syno-tag/syno-tag.component";
+import {SynoButtonComponent} from "./syno-button/syno-button.component";
+import {SynoEmailComponent} from "./syno-email/syno-email.component";
+import {SynoPasswordComponent} from "./syno-password/syno-password.component";
+import {SynoSwitchComponent} from "./syno-switch/syno-switch.component";
+import {FormsModule} from "@angular/forms";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {TablerIconsModule} from "angular-tabler-icons";
+import {IconChevronDown, IconChevronUp} from "angular-tabler-icons/icons";
 
-
+const icons = {
+  IconChevronDown,
+  IconChevronUp,
+};
 
 @NgModule({
   declarations: [
-    CoreUiLibComponent,
-    TestComponent
+    SynoButtonComponent,
+    SynoEmailComponent,
+    SynoPasswordComponent,
+    SynoUsernameComponent,
+    SynoIconButtonComponent,
+    SynoSwitchComponent,
+    SynoTagComponent,
+    SynoSelectComponent,
+    SynoSelectMultipleComponent,
   ],
   imports: [
+    FormsModule,
+    NgClass,
+    TablerIconsModule.pick(icons),
+    NgIf,
+    NgForOf,
   ],
   exports: [
-    CoreUiLibComponent,
-    TestComponent,
+    SynoButtonComponent,
+    SynoEmailComponent,
+    SynoPasswordComponent,
+    SynoUsernameComponent,
+    SynoIconButtonComponent,
+    SynoSwitchComponent,
+    SynoTagComponent,
+    SynoSelectComponent,
+    SynoSelectMultipleComponent,
   ]
 })
-export class CoreUiLibModule { }
+export class CoreUiLibModule {
+}
--- projects/core-ui-lib/src/public-api.ts
+++ projects/core-ui-lib/src/public-api.ts
@@ -2,8 +2,14 @@
  * Public API Surface of core-ui-lib
  */
 
-export * from './lib/core-ui-lib.service';
-export * from './lib/core-ui-lib.component';
-export * from './lib/core-ui-lib.module';
+export * from "./lib/core-ui-lib.module";
 
-export * from "./lib/test/test.component";
+export * from "./lib/syno-button/syno-button.component";
+export * from "./lib/syno-email/syno-email.component";
+export * from "./lib/syno-icon-button/syno-icon-button.component";
+export * from "./lib/syno-password/syno-password.component";
+export * from "./lib/syno-select/syno-select.component";
+export * from "./lib/syno-select-multiple/syno-select-multiple.component";
+export * from "./lib/syno-switch/syno-switch.component";
+export * from "./lib/syno-tag/syno-tag.component";
+export * from "./lib/syno-username/syno-username.component";
